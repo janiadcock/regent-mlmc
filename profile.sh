029d6918581e42b3a25819bf0a86fcb9de282587
Manolis Papadakis <manopapad@gmail.com>
2018-08-29 15:49:00 -0600

    Clean up scripts

--- profile.sh
+++ profile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -eu
 
-$LEGION_DIR/tools/legion_prof.py prof_*.log
-firefox legion_prof/index.html
+"$LEGION_DIR"/tools/legion_prof.py prof_*.log
+xdg-open legion_prof/index.html
--- run.sh
+++ run.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -eu
 
 export INCLUDE_PATH=.
-export LD_LIBRARY_PATH=.:$LEGION_DIR/language/hdf/install/lib/
+export LD_LIBRARY_PATH=.
+if [[ ! -z "${HDF_ROOT:-}" ]]; then
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HDF_ROOT/lib"
+fi
 
 gcc -Wall -Wextra -pedantic -g -fPIC -c diffusion.c
 gcc -shared -o libdiffusion.so diffusion.o
 rm -rf prof_*.log legion_prof/
-$LEGION_DIR/language/regent.py mlmc.rg -ll:cpu 3 -lg:prof 1 -lg:prof_logfile prof_%.log -lg:sched -1
+"$LEGION_DIR"/language/regent.py mlmc.rg -ll:cpu 3 -lg:prof 1 -lg:prof_logfile prof_%.log -lg:sched -1
